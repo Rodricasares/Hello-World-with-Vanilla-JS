b1e85cf492f049cb3721e83029ffae72496dfecc
Rodricasares <rodrigocantudo@gmail.com>
2022-01-15 11:17:31 +0000

    Esto esta subido para corregir

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let didWho = Math.floor(Math.random() * who.length);
+  let doAction = Math.floor(Math.random() * action.length);
+  let placeWhat = Math.floor(Math.random() * what.length);
+  let timeWhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[didWho] +
+    " " +
+    action[doAction] +
+    " " +
+    what[placeWhat] +
+    " " +
+    when[timeWhen]
+  );
 };
